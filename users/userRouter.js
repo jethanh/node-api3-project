99dc52c144db96692585339dca0f9dde46d3187a
unknown <jethanhargrave@gmail.com>
2020-05-08 06:15:29 -0400

    put and delete

--- users/userRouter.js
+++ users/userRouter.js
@@ -57,11 +57,38 @@ router.get('/:id/posts', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
+  const id = req.params.id
+  User.remove(id)
+    .then(res.status(200).end())
+    .catch(err => {
+      res.status(500).json({ errorMessage: "Somethin' went wrong, bucko." })
+    })
   
 });
 
 router.put('/:id', (req, res) => {
-  // do your magic!
+  //only ID and Name in this endpoint, PUT updates name, uses ID to determine which user's name to update.
+  //How would we do this if there are multiple things to update like a bio? something like {name, bio} or multiple arguments?
+  const id = req.params.id
+  const name = req.body.name
+  User.update(id, name)
+    .then(newInfo => {
+      if(newInfo){
+        User.getById(id)
+        .then(item => {
+          res.status(200).json(item)
+        })
+        .catch(err => {
+          res.status(500).json({ errorMessage: "Somethin' went wrong, bucko." })
+        })
+      } else {
+        res.status(500).json({ errorMessage: "Somethin' went wrong, bucko." })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ errorMessage: "Somethin' went wrong, bucko." })
+    })
+
 });
 
 //custom middleware
